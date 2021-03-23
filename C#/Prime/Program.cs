0da308b81bb6ee645430d802f645c15d5ded74d9
Truong Cong Vinh <duongcongvinh18122001@gmail.com>
2021-03-23 23:22:22 +0700

    Return Prime  - haven't used asynchronous programming yet

---
+++ C#/Prime/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace Prime
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Prime Numbers between 1 to 100 : ");
+             for (int i = 2; i <= 100; i++)
+             {
+                 if (IsPrime(i))
+                 {
+                     Console.Write("\t" + i); 
+                 }
+            }
+             Console.ReadKey();
+        }
+        static async Task<bool> IsPrime(int num){
+                for (int i = 2; i < Math.Sqrt(num); i++)// base logic for the primality
+                 {
+                     if (num % i == 0) //modulo operators employed
+                    {
+                          return false;
+                    }
+                 }
+                 return true;
+        }
+    }
+}
