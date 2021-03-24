c83387c2afeab7c5d47fe70dbc3c969357326f7c
Truong Cong Vinh <duongcongvinh18122001@gmail.com>
2021-03-24 17:50:04 +0700

    Finish Middleware

--- C#/Middleware/Custommiddleware.cs
+++ C#/Middleware/Custommiddleware.cs
@@ -2,6 +2,7 @@ using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
+using System.Diagnostics;
 namespace Middleware
 {
 
@@ -15,7 +16,16 @@ namespace Middleware
 
         public async Task Invoke(HttpContext httpContext)
         {
-            await httpContext.Response.WriteAsync("<h1> Welcome !</h1>");
+            var sw = new Stopwatch();
+            sw.Start();
+            var name = httpContext.Request.Query["name"];
+
+            if (!String.IsNullOrWhiteSpace(name) )
+            {
+                httpContext.Request.Headers.Add("name",name);
+            }
+            await httpContext.Response.WriteAsync($"<p>{httpContext.Request.Path} {httpContext.Request.QueryString}</p>");
+            await httpContext.Response.WriteAsync($"<h1> {name} </h1><h2>{sw.ElapsedMilliseconds}</h2>");
             await _next(httpContext);
         }
     }
