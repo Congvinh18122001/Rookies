8e4accd7cc5f2325c33e672690a074bf8b2ba5e5
Truong Cong Vinh <duongcongvinh18122001@gmail.com>
2021-04-08 22:24:12 +0700

    practice reactjs day 5

--- C#/Middleware/Startup.cs
+++ C#/Middleware/Startup.cs
@@ -48,12 +48,8 @@ namespace Middleware
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-            app.Map("/vinh", HandleMapHello);
             app.UseCustommiddleware();
-            app.Run(async context =>
-            {
-                await context.Response.WriteAsync("<div>Welcome from ASP.NET Core!</div>");
-            });
+
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
@@ -65,7 +61,11 @@ namespace Middleware
             {
                 endpoints.MapRazorPages();
             });
-
+            // app.Map("/vinh", HandleMapHello);
+            // app.Run(async context =>
+            // {
+            //     await context.Response.WriteAsync("<div>Welcome from ASP.NET Core!</div>");
+            // });
            
        }
   }
